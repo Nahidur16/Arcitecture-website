1770a30a4df50c4f9c2a2494fbfefa2ea170cd00
Nahidur <naarnab@gmail.com>
2020-12-08 04:06:31 +0600

    Complete (Not Responsive)

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 //jshint esversion:6
+// Side menu
 let menu = document.querySelector(".hamburger-menu");
 menu.addEventListener("click", () => {
   let container = document.querySelector(".container");
   console.log("clicked");
   container.classList.toggle("change");
+});
+
+//Scrill top
+let scrollBtn = document.querySelector(".scroll-btn");
+scrollBtn.addEventListener("click", () => {
+  let html = document.querySelector("html");
+  html.style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    html.style.scrollBehavior = "unset";
+  }, 1000);
 });
